f3cbf0991f3d47ed529c02f0c1217eca0165be34
Márk Bartha <barthamark@gmail.com>
2026-04-04 17:12:41 +0200

    Simplify Tailwind targets sample tests

--- Lombiq.Tailwind.Targets.Tests/TailwindTargetsSample.cs
+++ Lombiq.Tailwind.Targets.Tests/TailwindTargetsSample.cs
@@ -1,6 +1,4 @@
 using System.IO;
-using System.Linq;
-using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -9,20 +7,11 @@ namespace Lombiq.Tailwind.Targets.Tests;
 internal static class TailwindTargetsSample
 {
     public static string GeneratedCssPath =>
-        Path.Combine(GetSampleProjectDirectory(), "wwwroot", "css", "site.css");
+        Path.Join("wwwroot", "css", "site.css");
 
     public static bool GeneratedCssExists() =>
         File.Exists(GeneratedCssPath);
 
     public static Task<string> ReadGeneratedCssAsync(CancellationToken cancellationToken) =>
         File.ReadAllTextAsync(GeneratedCssPath, cancellationToken);
-
-    private static string GetSampleProjectDirectory() =>
-        Path.GetFullPath(Path.Combine(
-            typeof(TailwindTargetsSample).Assembly
-                .GetCustomAttributes<AssemblyMetadataAttribute>()
-                .Single(attribute => attribute.Key == "MSBuildProjectDirectory")
-                .Value,
-            "..",
-            "Lombiq.Tailwind.Targets.Samples"));
 }
